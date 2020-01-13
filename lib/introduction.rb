3efdb0a99493e63c0846839a20f30b743c34f7fb
Harley McDowell <hmcdowell95@gmail.com>
2020-01-13 02:04:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,9 +4,12 @@
 # in order to complete the rest of this course and be ready for the next course!
 def introduction(name)
   puts "Hi, my name is #{name}."
-introduction = "Hi, my name is #{name}."
-introduction_with_language = "Hi, my name is #{name} and I am learning to program in #{language}."
-introduction_with_language_optional = "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
 
 
 
